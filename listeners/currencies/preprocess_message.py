da280b202bb35bb870e44c43aad624092cee488c
KiGamji <maloyh44@gmail.com>
2023-04-19 10:31:17 +0500

    currencies/preprocess_message: Remove useless import

--- listeners/currencies/preprocess_message.py
+++ listeners/currencies/preprocess_message.py
@@ -1,7 +1,5 @@
 import re
 
-from .triggers import *
-    
 def preprocess_message(message: str) -> str:
     message = re.sub(r'(\d)([^\d\s.,])', r'\1 \2', message)
     message = re.sub(r'([^\d\s.,])(\d)', r'\1 \2', message)
