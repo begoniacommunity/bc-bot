89c959862850b4ae4f89c620d131387aa875977e
KiGamji <maloyh44@gmail.com>
2023-04-19 10:33:42 +0500

    fictional_currencies: Move fictional_triggers to fictional_currency_triggers

--- listeners/fictional_currencies/__init__.py
+++ listeners/fictional_currencies/__init__.py
@@ -1,6 +1,6 @@
 from .convert_fictional_currency import *
 from .fictional_currency_emojis import *
-from .fictional_triggers import *
+from .fictional_currency_triggers import *
 from .get_fictional_rates import *
 from .preprocess_message import *
 from .react_chances import *
