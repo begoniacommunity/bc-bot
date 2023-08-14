954a5339b1c48f826f6e3aa3ae06efb15382cf0e
KiGamji <maloyh44@gmail.com>
2023-08-15 04:08:57 +0500

    feat: rework preprocess_message
    - Ignore mentions
    - Ignore http(s), www. and .com links
    - Support numbers with spaces between classes

--- handlers/currencies/preprocess_message.py
+++ handlers/currencies/preprocess_message.py
@@ -1,6 +1,7 @@
 import re
 
 def preprocess_message(message: str) -> str:
-    message = re.sub(r'(\d)([^\d\s.,])', r'\1 \2', message)
-    message = re.sub(r'([^\d\s.,])(\d)', r'\1 \2', message)
+    message = re.sub(r"@(\S+)", "", message)
+    message = re.sub(r"\b(?:https?:\/\/|www\.)\S+\b|\b\S+\.com\S*\b", "", message)
+    message = re.sub(r"(?<!\d)(\d{1,3}(?: \d{3})*(?:\.\d+)?)(?=(?:\s\d{3})*(?:\.\d+)?|\D|$)", lambda match: match.group(1).replace(" ", ""), message)
     return message
