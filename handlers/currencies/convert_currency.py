086f42ae880574735f42e7b7113041bb4bc4762e
KiGamji <maloyh44@gmail.com>
2023-12-02 23:56:46 +0500

    refactor(currencies): move preprocess_message to convert_currency

--- handlers/currencies/convert_currency.py
+++ handlers/currencies/convert_currency.py
@@ -5,7 +5,12 @@ from .currency_emojis import *
 from .currency_triggers import *
 from .get_rates import *
 from .main_currencies import *
-from .preprocess_message import *
+
+def preprocess_message(message: str) -> str:
+    message = re.sub(r"@(\S+)", "", message)
+    message = re.sub(r"\b(?:https?:\/\/|www\.)\S+\b|\b\S+\.com\S*\b", "", message)
+    message = re.sub(r"(?<!\d)(\d{1,3}(?: \d{3})*(?:\.\d+)?)(?=(?:\s\d{3})*(?:\.\d+)?|\D|$)", lambda match: match.group(1).replace(" ", ""), message)
+    return message
 
 async def convert_currency(message: Message):
     if not message.text:
--- handlers/currencies/preprocess_message.py
+++
@@ -1,7 +0,0 @@
-import re
-
-def preprocess_message(message: str) -> str:
-    message = re.sub(r"@(\S+)", "", message)
-    message = re.sub(r"\b(?:https?:\/\/|www\.)\S+\b|\b\S+\.com\S*\b", "", message)
-    message = re.sub(r"(?<!\d)(\d{1,3}(?: \d{3})*(?:\.\d+)?)(?=(?:\s\d{3})*(?:\.\d+)?|\D|$)", lambda match: match.group(1).replace(" ", ""), message)
-    return message
